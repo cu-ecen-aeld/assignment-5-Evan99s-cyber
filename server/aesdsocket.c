602a4c2e31185fe455f60447bd68236761d88a7d
Evan99s-cyber <evsp9901@colorado.edu>
2026-04-24 00:32:56 -0400

    Update aesdsocket.c
    logic update

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,7 +11,6 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define PORT "9000"
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 1024
 
@@ -22,15 +21,14 @@ void cleanup() {
     if (client_fd != -1) close(client_fd);
     if (server_fd != -1) close(server_fd);
     unlink(DATA_FILE);
+    syslog(LOG_INFO, "Cleanup complete, closing log.");
     closelog();
 }
 
 void signal_handler(int sig) {
-    if (sig == SIGINT || sig == SIGTERM) {
-        syslog(LOG_INFO, "Caught signal, exiting");
-        cleanup();
-        exit(0);
-    }
+    syslog(LOG_INFO, "Caught signal, exiting");
+    cleanup();
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -38,41 +36,23 @@ int main(int argc, char *argv[]) {
 
     // Setup Signal Handling
     struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
     sa.sa_handler = signal_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
     if (sigaction(SIGINT, &sa, NULL) == -1 || sigaction(SIGTERM, &sa, NULL) == -1) {
         perror("sigaction");
         return -1;
     }
 
-    // Inside main, before socket setup:
-int daemon_mode = 0;
-if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-    daemon_mode = 1;
-}
-
-// ... setup socket, bind, and listen FIRST ...
-
-if (daemon_mode) {
-    if (daemon(0, 0) == -1) {
-        syslog(LOG_ERR, "Failed to enter daemon mode");
-        return -1;
-    }
-}
-
-    // 1. Create Socket
+    // 1. Setup Socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
         return -1;
     }
 
-    // Allow port reuse
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // 2. Bind to Port 9000
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -85,33 +65,18 @@ if (daemon_mode) {
         return -1;
     }
 
-    // 3. Listen
+    // 2. Daemonize ONLY after bind (so parent exits only if bind succeeds)
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        if (daemon(0, 0) == -1) {
+            syslog(LOG_ERR, "Daemonization failed: %s", strerror(errno));
+            return -1;
+        }
+    }
+
     if (listen(server_fd, 10) == -1) {
         syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
         close(server_fd);
         return -1;
-    }
-
-// Insert this between lines 78 and 80
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        pid_t pid = fork();
-        if (pid < 0) {
-            syslog(LOG_ERR, "Fork failed");
-            return -1;
-        }
-        if (pid > 0) {
-            // Parent exits to return control to shell/GitHub runner
-            exit(0);
-        }
-        // Child continues - redirect standard streams
-        if (setsid() < 0) return -1;
-        if (chdir("/") < 0) return -1;
-        
-        int dev_null = open("/dev/null", O_RDWR);
-        dup2(dev_null, STDIN_FILENO);
-        dup2(dev_null, STDOUT_FILENO);
-        dup2(dev_null, STDERR_FILENO);
-        close(dev_null);
     }
 
     while (1) {
@@ -128,7 +93,7 @@ if (daemon_mode) {
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // 4. Receive data and append to file
+        // 3. Receive and Append
         FILE *fp = fopen(DATA_FILE, "a+");
         if (!fp) {
             syslog(LOG_ERR, "File open failed");
@@ -136,46 +101,33 @@ if (daemon_mode) {
             continue;
         }
 
-        char *buffer = malloc(BUFFER_SIZE);
-        if (!buffer) {
-            syslog(LOG_ERR, "Malloc failed");
-            fclose(fp);
-            close(client_fd);
-            continue;
-        }
-
+        char buffer[BUFFER_SIZE];
         ssize_t bytes_recv;
+        
+        // Single loop to handle stream until newline
         while ((bytes_recv = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0) {
             fwrite(buffer, 1, bytes_recv, fp);
-            
-            // Check if packet is complete (ends in newline)
             if (memchr(buffer, '\n', bytes_recv)) {
                 break; 
             }
         }
-
-        while ((bytes_recv = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0) {
-    fwrite(buffer, 1, bytes_recv, fp);
-    if (memchr(buffer, '\n', bytes_recv)) {
-        fflush(fp); // CRITICAL: Force the data onto the disk
-        break; 
-    }
-}
         
-        // 5. Send full file content back to client
+        fflush(fp);
+
+        // 4. Reset file pointer and Send everything back
         fseek(fp, 0, SEEK_SET);
-        size_t bytes_read;
         char read_buf[BUFFER_SIZE];
+        size_t bytes_read;
         while ((bytes_read = fread(read_buf, 1, BUFFER_SIZE, fp)) > 0) {
             send(client_fd, read_buf, bytes_read, 0);
         }
 
-        free(buffer);
         fclose(fp);
         close(client_fd);
         client_fd = -1;
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
     }
 
+    cleanup();
     return 0;
 }
